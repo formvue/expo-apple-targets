850793928a22c2d99e4feff500e2cbed29b993df
Ryan Floyd <ryandfloyd@gmail.com>
2025-09-30 08:59:17 -0700

    rev

--- packages/apple-targets/src/icon/withIosIcon.ts
+++ packages/apple-targets/src/icon/withIosIcon.ts
@@ -250,7 +250,8 @@ export async function generateIMessageIconsInternalAsync(
     { size: "32x24", scales: [2, 3], idiom: "universal", platform: "ios" },
     { size: "1024x768", scales: [1], idiom: "universal", platform: "ios" },
     { size: "1024x768", scales: [1], idiom: "ios-marketing", platform: "ios" },
-    { size: "1024x1024", scales: [1], idiom: "ios-marketing" },
+    // Marketing icon for App Store - use universal idiom for Messages Extensions so it gets extracted
+    { size: "1024x1024", scales: [1], idiom: "universal", platform: "ios" },
   ];
 
   for (const iconSize of iconSizes) {
